74b4b29e995c6bec758d1957b9a0d580e39b5393
Михаил Ким <mikim123123123123@gmail.com>
2020-05-15 01:35:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Lab7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.main_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // main_button
+            // 
+            this.main_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.main_button.AutoSize = true;
+            this.main_button.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.main_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.main_button.Location = new System.Drawing.Point(327, 187);
+            this.main_button.Name = "main_button";
+            this.main_button.Size = new System.Drawing.Size(57, 23);
+            this.main_button.TabIndex = 0;
+            this.main_button.Text = "Click me";
+            this.main_button.UseVisualStyleBackColor = true;
+            this.main_button.Click += new System.EventHandler(this.main_button_Click);
+            this.main_button.MouseMove += new System.Windows.Forms.MouseEventHandler(this.main_button_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(750, 435);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.main_button);
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "Form1";
+            this.Text = "Running button";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button main_button;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_1
+{
+    public partial class Form1 : Form
+    {
+
+        private int prevX = -1;
+
+        private int prevY = -1;
+
+        private double distance = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void main_button_Click(object sender, EventArgs e)
+        {
+            if (ShowDialog("Поздравляем! Вы смогли нажать на кнопку!", "Поздравляем!")) {
+                System.Windows.Forms.Application.Exit();
+            };
+        }
+
+        public static bool ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, AutoSize = true };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK;
+        }
+
+        private void main_button_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (prevX != -1 || prevY != -1)
+            {
+                int deltaX = Math.Abs(e.X - prevX);
+                int deltaY = Math.Abs(e.Y - prevY);
+                double newDistance = Math.Sqrt(Math.Pow(e.X - main_button.Location.X, 2) + Math.Pow(e.Y - main_button.Location.Y, 2));
+                var t = 1.01 + ((deltaX + deltaY) * 0.001);
+                if (deltaY < 10 && deltaY > 0 && newDistance < distance)
+                {
+                    //int newX = main_button.Location.X + deltaX;
+                    //int newY = main_button.Location.Y + deltaY;
+                    var newY = (1 - t) * e.Y + t * main_button.Location.Y;
+                    if (newY > 0 && (newY < (Height - main_button.Size.Height * 2.5)))
+                    {
+                        main_button.Location = new Point(main_button.Location.X, Convert.ToInt32(newY));
+                        textBox1.Text = $"{deltaX};{deltaY};{distance}";
+                    }
+                    else {
+                        textBox1.Text = "Stopped for y";
+                    }
+                    
+                }
+                if (deltaX > 0 && deltaX < 10 && newDistance < distance)
+                {
+                    Double newX = ((1 - t) * e.X + t * main_button.Location.X);
+                    if (newX > 0 && (newX < Width - main_button.Size.Width* 1.25)) {
+                        main_button.Location = new Point(Convert.ToInt32(newX), main_button.Location.Y);
+                    }
+                    
+                }
+                distance = newDistance;
+            }
+            
+            prevX = e.X;
+            prevY = e.Y;
+        }
+    }
+}
